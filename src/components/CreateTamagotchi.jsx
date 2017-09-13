5a0cb2346b380418660c187f483d4e5b67b8135b
Brittany Kerr and jacob ruleaux <jacob@>
2017-09-13 13:22:47 -0700

    build out create tama comp

--- src/components/CreateTamagotchi.jsx
+++ src/components/CreateTamagotchi.jsx
@@ -1,3 +1,37 @@
 import React from "react";
+import Tamagotchi from "../models/Tamagotchi.js";
+import PropTypes from "prop-types";
 
+class CreateTamagotchi extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.newCreateTamagotchi = this.newCreateTamagotchi.bind(this);
+  }
+
+  newCreateTamagotchi(event) {
+    event.preventDefault()
+    const { _name} = this.refs;
+    var newTamagotchi = new Tamagotchi(_name.value);
+    this.props.onNewCreateTamagotchi(new Tamagotchi);
+  }
+
+  render(){
+    return (
+      <div>
+        <form onSubmit={this.newCreateTamagotchi}>
+          <input
+            ref="_name"
+            type="text"
+            id="name"
+            placeholder="Tamagotchi Name"/>
+        </form>
+      </div>
+    );
+  }
+
+  CreateTamagotchi.propTypes = {
+    onNewCreateTamagotchi: PropTypes.func
+  }
+}
 export default CreateTamagotchi;
