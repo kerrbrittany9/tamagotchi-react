bb848bd9ab242a30bec54197c7a353ad614726a9
Brittany Kerr, jacob ruleaux and Michelle Poterek <michelle@>
2017-09-14 09:50:06 -0700

    add moment to model

--- src/models/Tamagotchi.js
+++ src/models/Tamagotchi.js
@@ -1,6 +1,15 @@
+import Moment from "moment";
+
 class Tamagotchi {
   constructor(name) {
-    this.name = name
+    this.name = name;
+    this.life = 100;
+    this.timeBirth= new Moment();
+    this.timeSinceBirth = this.setTimeSinceBirth();
+  }
+
+  setTimeSinceBirth() {
+    return this.timeBirth.fromNow(true);
   }
 }
 export default Tamagotchi;
